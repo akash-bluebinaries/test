2e7f31478c016efd8a24a35fd8693813c80844a6
akash-bluebinaries <akash.dash@bluebinaries.com>
2024-07-03 14:05:37 +0530

    ingestion and transformation

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
 
-name = 'mlproject',
+name = 'abc',
     version = '0.0.1',
     author = 'akash',
     author_email='akash.dash@bluebinaries.com',
